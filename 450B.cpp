f43cae4f915a8b7508199c9271c7e56b79504edd
Socerer <coolfool@socerer>
2022-09-29 00:00:10 +0530

    commit to save

---
+++ 450B.cpp
@@ -0,0 +1,11 @@
+// Modulo real-world application example: 12-Hour clock uses (24H-Time modulo 12) like 14:00 modulo 12 = 2PM
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	
+
+	return 0;
+}
